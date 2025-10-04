7bc00013b25f5870d880db5ce48e59d7935a6e2e
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2025-10-03 21:04:30 -0300

    Create default

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,18 @@
 import Head from 'next/head';
 import styles from '@/styles/globals.css';
+
+export default function Home(){
+    return(
+        <main>
+            <Head>
+                <title>Loja de Oculos</title>
+                <meta name="description" content="Loja de Oculos, Aqui voce encontra o melhor Oculos" />
+                <meta name="viewport" content="width=device-width, inicial-scale=1" />
+                <link rel="" href="" />
+            </Head>
+            <main>
+                
+            </main>
+        </main>
+    );
+}
