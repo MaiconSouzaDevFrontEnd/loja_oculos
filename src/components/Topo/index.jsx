11602b4b93e829d675e48a042065e8689a8b62b2
MaiconSouzaDevFrontEnd <you@example.com>
2025-10-07 07:00:25 -0300

    erro acontecendo estou tentando IMAGE

--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -1,10 +1,14 @@
-import Image from 'next/Image';
+import Image from 'next/image';
 import Logo from '@/publico/logo.png';
+
+// Importando o CSS do componente   
+import styles from './Topo.module.css';     
+
 export default function Topo(){
     return(
-        <header>
-            <Image src={Logo} alt="logomarca" />
-            <nav>
+        <header className={styles.topo}>
+            <Image src={Logo} alt="logomarca" className={styles.logomarca}/>
+            <nav className={styles.navmenu}>
                 <a href="#produtos">Produtos</a>
                 <a href="#sobre">Sobre</a>
                 <a href="#contatos">Contatos</a>
