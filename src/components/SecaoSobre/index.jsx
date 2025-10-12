484cd21975bab96cb7a263e3c87a065b237d1356
MaiconSouzaDevFrontEnd <you@example.com>
2025-10-12 15:31:08 -0300

    ajustando cards produtos e sobre

--- src/components/SecaoSobre/index.jsx
+++ src/components/SecaoSobre/index.jsx
@@ -1,10 +1,30 @@
 import styles from './SecaoSobre.module.css';
+import Image from 'next/image';
+import loja from '@/publico/loja.png';
+import atendimento from '@/publico/atendimento.png';
 
 export default function SecaoSobre() {
   return (
-    <section id="sobre" className={styles.secaoSobre ?? ''}>
-      <h2>Sobre</h2>
-      <p>Informações sobre a loja.</p>
+    <section id="sobre" className={styles.secaoSobre}>
+      <h2>QUEM SOMOS NÓS?</h2>
+      <p>Fundada em 2001, em Nova Iguaçu - Rio de Janeiro, a Óticas vida iniciou suas atividades focada no atendimento ao público de renda mais baixa, sempre com o objetivo de proporcionar ao cliente bom atendimento, qualidade e preço baixo. </p>
+      <div className={styles.containerSobre}>
+          <article>
+            <Image src={loja} alt='Mostroario de uma farmácia onde uma farmaceutica retira uma caixa de medicamentos' className={styles.img} />
+          </article>
+          <div>
+            <h3>Nossa filiais</h3>
+            <p>Hoje temos mais de 20 filiais pelo Brasil e na América</p>
+          </div>
+           <div>
+            <h3>Atendimento flexível</h3>
+            <p>Nossa equipe possui é treinada para te atender</p>
+          </div>
+          <article>
+            <Image src={atendimento} alt='atendimento na farmácia' className={styles.img} />
+          </article>
+         
+      </div>
     </section>
   );
 }
--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Logo from '@/publico/logo.png';
+import Link from 'next/link';
 
 // Importando o CSS do componente   
 import styles from './Topo.module.css';     
@@ -7,12 +8,13 @@ import styles from './Topo.module.css';
 export default function Topo(){
     return(
         <header className={styles.topo}>
-            <Image src={Logo} alt="logomarca" className={styles.logomarca} />
+            <Image src={Logo} alt="logomarca" className={styles.logoMarca} />
             <nav className={styles.navmenu}>
-                <a href="#produtos">Produtos</a>
-                <a href="#sobre">Sobre</a>
-                <a href="#contatos">Contatos</a>
+                <Link href="#produtos">Produtos</Link>
+                <Link href="#sobre">Sobre</Link>
+                <Link href="#contatos">Contatos</Link>
             </nav>
         </header>    
-        );}
+        );
+    }
         
