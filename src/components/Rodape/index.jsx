d1dd962b7ba3e9e02189146521fd7c431b118de3
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2025-10-20 12:35:39 -0300

    criando container

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -2,8 +2,17 @@ import styles from './Rodape.module.css';
 
 export default function Rodape() {
 	return (
-		<footer className={styles.rodape ?? ''}>
-			<p>© {new Date().getFullYear()} Loja de Óculos - Todos os direitos reservados.</p>
+		<footer className={styles.rodape}>
+			<div className={styles.container}>
+				<h3 className={styles.titulo}>Fale conosco</h3>
+				<p className={styles.paragrafo_rodape}>Não perca tempo, venha conhecer uma de nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento.</p>
+				
+				<section className={styles.subContainer}>
+					
+				</section>
+				
+				<p>© {new Date().getFullYear()} Loja de Óculos - Todos os direitos reservados.</p>
+			</div>
 		</footer>
 	);
 }
